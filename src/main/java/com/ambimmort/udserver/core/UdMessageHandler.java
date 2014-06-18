6fff474b25b33a7666b607be6d702069284c4f81
hedingwei <hedingwei@ambimmort.com>
2014-06-18 14:38:20 +0800

    t5

--- src/main/java/com/ambimmort/udserver/core/UdMessageHandler.java
+++ src/main/java/com/ambimmort/udserver/core/UdMessageHandler.java
@@ -5,6 +5,8 @@
  */
 package com.ambimmort.udserver.core;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import org.apache.mina.core.service.IoHandlerAdapter;
 import org.apache.mina.core.session.IdleStatus;
 import org.apache.mina.core.session.IoSession;
@@ -16,6 +18,18 @@ import org.apache.mina.core.session.IoSession;
 public class UdMessageHandler extends IoHandlerAdapter {
 
     UdMessageCache cache = UdMessageCache.getInstance();
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+    @Override
+    public void sessionOpened(IoSession session) throws Exception {
+        System.out.println("dpi session opend[" + sdf.format(new Date(session.getCreationTime())) + "].  remote address: " + session.getRemoteAddress());
+    }
+
+    @Override
+    public void sessionClosed(IoSession session) throws Exception {
+        System.out.println("dpi session opend[" + sdf.format(new Date(session.getCreationTime())) + "].  remote address: " + session.getRemoteAddress()+"\t lastIoTime:["+ sdf.format(new Date(session.getLastIoTime()))+"]");
+        
+    }
 
     @Override
     public void exceptionCaught(IoSession session, Throwable cause) throws Exception {
